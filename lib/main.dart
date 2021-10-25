08c21364ee5ebb1aa2f4888c742f9cd91ec76899
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2021-10-25 19:25:08 +0500

    Trave_App_Ui

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:travel_app_ui/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: "Travel App UI",
+      debugShowCheckedModeBanner:false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+      ),
+      home: HomeScreen(),
+      
+    );
+  }
+}
